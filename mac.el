b07968212870ae99ca4d2448e83944bdcca878ae
Pramod <neppramod@gmail.com>
2020-11-03 16:24:03 -0700

    Add latest setting, after testing in Mac

---
+++ mac.el
@@ -0,0 +1,3 @@
+(setenv "JAVA_HOME"
+        "/Users/pramod.nepal/Documents/lib/jdk-11_0_8_10/Contents/Home/")
+(setq lsp-java-java-path "/Users/pramod.nepal/Documents/lib/jdk-11_0_8_10/Contents/Home/bin/java")
